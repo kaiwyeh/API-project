6c685d705e1f73859b51713ad41637f867013fb4
kyeh <kyehpersonal@gmail.com>
2022-09-21 16:06:36 -0700

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -20,3 +20,10 @@ router.get("/api/csrf/restore", (req, res) => {
  });
 });
 // ...
+
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
