f8668dc9deeb578ac065669d793392a2188009c5
kyeh <kyehpersonal@gmail.com>
2022-09-30 21:17:09 -0700

    working on spot-images route

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -5,34 +5,6 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
-router.delete(
- '/:imageId', requireAuth, async (req, res, next) => {
-  const { imageId } = req.params;
-  const { user } = req;
-  const image = await SpotImage.findByPk(imageId);
-
-  if (!image) {
-   res.status(404)
-   return res.json({
-    message: "Spot Image couldn't be found",
-    statusCode: 404
-   })
-  }
-
-  const spot = await Spot.findByPk(image.spotId)
-
-  if (spot.ownerId === user.id) {
-   await image.destroy()
-   res.status(200)
-   return res.json({
-    "message": "Successfully deleted",
-    "statusCode": 200
-   })
-  } else {
-   await requireAuthRole(req, res, next);
-  }
- }
-)
 
 
 
