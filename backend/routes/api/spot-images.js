b88ec2b859c7f0d45670087e691d0135858bd584
kyeh <kyehpersonal@gmail.com>
2022-10-01 11:42:45 -0700

    spot-images done

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const { Booking, User, Spot, Review, SpotImage, ReviewImage, Sequelize } = require('../../db/models');
-const { setTokenCookie, requireAuth, restoreUser, requireAuthRole } = require('../../utils/auth');
+const { setTokenCookie, requireAuth, restoreUser, requireAuthRole, requireAuthorization } = require('../../utils/auth');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+ const { imageId } = req.params;
+ const { user } = req;
+ const findImage = await SpotImage.findByPk(imageId);
+ const findSpot = await Spot.findByPk(findImage.spotId)
+
+ if (!findImage) {
+  res.status(404)
+  return res.json({
+   "message": "Spot Image couldn't be found",
+   "statusCode": 404
+  })
+ }
+
+ if (findSpot.ownerId === user.id) {
+  await findImage.destroy()
+  res.status(200)
+  return res.json({
+   "message": "Successfully deleted",
+   "statusCode": 200
+  })
+ }
+
+ if (findSpot.ownerId !== user.id) {
+  await requireAuthorization(req, res, next);
+ }
+
+})
 
 
 
