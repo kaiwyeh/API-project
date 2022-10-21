de23e95f095fc338a7e8b66c0f9ff6a204962f17
kyeh <kyehpersonal@gmail.com>
2022-10-21 14:09:31 -0700

    changed review feature

--- frontend/src/components/Reviews/ReviewsDeletePage.js
+++ frontend/src/components/Reviews/ReviewsDeletePage.js
@@ -1,27 +1,2 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom'
+
 import { deleteReviewById } from '../../store/reviews';
-
-import './ReviewsDeletePage.css'
-
-
-
-const ReviewsDeletePage = ({ reviewId, spotId }) => {
- const dispatch = useDispatch();
- const history = useHistory();
- 
-
- const review = useSelector(state => state.review[reviewId])
-
- const onClick = async (event) => {
-  event.preventDefault();
-  await dispatch(deleteReviewById(Number(review.id)))
-  history.push(`/spots/${spotId}`)
- }
-
- return (
-  <button className='deletereviewbutton' onClick={onClick}>Delete Review</button>
- )
-}
-
-export default ReviewsDeletePage;
