bb8790afbd5ca3e4896e742a5939437892331af7
kyeh <kyehpersonal@gmail.com>
2022-11-04 14:33:40 -0700

    changed seeder and readme

--- README.md
+++ README.md
@@ -1,21 +1,52 @@
-Welcome to TravelBnb!
+# Welcome to TravelBnb!
 
 TravelBnB is a web application clone inspired by Airbnb, that allows online users to rent homes and other types of properties for vacations and other kinds of activities.
+* [Live Site](https://travelbnb-kai.herokuapp.com/)
 
 List of technologies used on the project:
-JavaScript, React, Redux, Express, Sequelize, HTML, CSS
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB)
+![Sequelize](https://img.shields.io/badge/Sequelize-52B0E7?style=for-the-badge&logo=Sequelize&logoColor=white)
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
+![Redux](https://img.shields.io/badge/redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white)
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![Heroku](https://img.shields.io/badge/heroku-%23430098.svg?style=for-the-badge&logo=heroku&logoColor=white)
 
-Launch the application locally:
-Clone from GitHub repository and run npm install in both the frontend and backend directories.
-Create a ".env" file in the "backend" directory using the ".env.example" as a guide
-In the "backend" directory, run the migration and seeder files using the following commands: "npx dotenv sequelize db:migrate" "npx dotenv sequelize db:seed:all"
-Run "npm start" in both the "frontend" and "backend" directories
 
-Features:
-CRUD feature Spots (Create, Read, Update, AND Delete)
-feature Reviews
+# Features:
 
-User Authentication
-Sign Up
-Login / logout
-Demo User Login
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Launch the application locally:
+1. Clone this repository
+2. `cd` into the repository
+3. `npm install` to install dependencies in both frontend folder and backend folder.
+4. Create a `.env` file using the `.env.example` as a guide.
+    ```
+    PORT=
+    DB_FILE=db/dev.db
+    JWT_SECRET=
+    JWT_EXPIRES_IN=
+    ```
+5. Move to backend directory.
+6. Run `npx dotenv sequelize db:migrate`
+7. Run `npx dotenv sequelize db:seed:all`
+8. With two terminals, Run `npm start` in both frontend folder and backend folder to start the application.
--- backend/db/seeders/20220927211641-spot.js
+++ backend/db/seeders/20220927211641-spot.js
@@ -13,7 +13,7 @@ module.exports = {
         lat: 33.653785493254716,
         lng: - 117.90601033981734,
         name: 'House near the beach',
-        description: 'The House is ideal for romantic getaways or business travels. It is nestled in a calm backyard, in a safe residential area, among bamboos, aloes and agaves. Special attention was given to the comfy queen bed. The "business corner" features fast Wi-Fi and a laptop desk.',
+        description: ' The House is ideal for romantic getaways or business travels. It is nestled in a calm backyard, in a safe residential area, among bamboos, aloes and agaves. Special attention was given to the comfy queen bed. The "business corner" features fast Wi-Fi and a laptop desk.',
         price: 370,
       },
       {
@@ -25,7 +25,7 @@ module.exports = {
         lat: 33.612840767237834,
         lng: - 117.92898133263157,
         name: 'Beach House in the heart of Balboa Peninsula',
-        description: 'Come experience quintessential Newport Beach in your private cozy beach house. Spend your sunny days enjoying the outdoor patio or take a quick walk to the best California beaches for a day by the ocean. Spend warm nights BBQing at the house or walk over to Lido Marina Village to enjoy the finest waterfront eateries in town.',
+        description: ' Come experience quintessential Newport Beach in your private cozy beach house. Spend your sunny days enjoying the outdoor patio or take a quick walk to the best California beaches for a day by the ocean. Spend warm nights BBQing at the house or walk over to Lido Marina Village to enjoy the finest waterfront eateries in town.',
         price: 400,
       },
       {
@@ -37,7 +37,7 @@ module.exports = {
         lat: 34.0229584173266,
         lng: - 118.45300365953959,
         name: 'Beautiful house near Santa Monica Pier',
-        description: 'Modern 2 story house in Santa Monica. Mahogany wood floors, marble and granite throughout. Recessed lighting and rich decor.',
+        description: ' Modern 2 story house in Santa Monica. Mahogany wood floors, marble and granite throughout. Recessed lighting and rich decor.',
         price: 799,
       },
       {
@@ -49,7 +49,7 @@ module.exports = {
         lat: 33.662864354701306,
         lng: - 117.99869045615614,
         name: 'Huntington Beach, Amazing View',
-        description: 'You have to see the view from this place!',
+        description: ' You have to see the view from this place!',
         price: 435,
       },
       {
@@ -61,7 +61,7 @@ module.exports = {
         lat: 33.62523673609298,
         lng: - 117.91752602842305,
         name: '1 mile from Newport Beach',
-        description: 'Fantastic location with private corner lot and large backyard with JACUZZI & BBQ. Easy parking drive way 4 cars ,AC (entire bungalow)great for summer , bikes available,alarm , and security cameras system .Boat and jet skis available for additional cost.',
+        description: ' Fantastic location with private corner lot and large backyard with JACUZZI & BBQ. Easy parking drive way 4 cars ,AC (entire bungalow)great for summer , bikes available,alarm , and security cameras system .Boat and jet skis available for additional cost.',
         price: 435,
       }
     ], {});
